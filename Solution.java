0562214638a46431efec076d3f2a3ea7cef51220
Pranjalb-37 <pranjalbelokar@gmail.com>
2026-06-22 00:56:48 +0530

    `Removed commented out line "this is a leetcode problem" from Solution.java

--- Solution.java
+++ Solution.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 class Solution {
     static void main(String[] args) {
+        // this is a leetcode problem
         int[] nums = {4,3,2,7,8,2,3,1};
         System.out.println(findDisappearedNumbers(nums));
     }
